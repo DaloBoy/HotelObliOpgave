3bf59dd100a87dcd78872226e138b09430535324
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-03-29 12:40:13 +0200

    Oprette Solution Oprettet Model Classes Med properties

---
+++ HotelObliOpgave/Model/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelObliOpgave.Model
+{
+    class Booking
+    {
+        public int Booking_id { get; set; }
+        public int Hotel_No { get; set; }
+        public int Guest_No { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public int Room_No { get; set; }
+    }
+}
---
+++ HotelObliOpgave/Model/Hotel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelObliOpgave.Model
+{
+    class Hotel
+    {
+        public int Hotel_No { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
